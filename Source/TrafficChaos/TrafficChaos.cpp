cff31fe692fcaa53146905cb49524ca7c753fbd0
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2026-05-28 18:05:38 +0100

    Initial commit.

---
+++ Source/TrafficChaos/TrafficChaos.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TrafficChaos.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrafficChaos, "TrafficChaos" );
